0fd0f416b5b2c27f4e2210abf869ac0df7c85674
Zachary Mooney <zamo22@student.bth.se>
2024-10-11 11:01:05 +0200

    Change tailwind to normal CSS class for mediaqueries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,12 @@ export default function Home() {
         height={250}
         className="mx-auto rounded-full"
       />
-      <h2 className="font-mono text-xl flex mt-5 items-center justify-center">Zachary Mooney | Software Developer/Web Programmer</h2>
-      <h1 className="font-mono text-9xl flex items-center justify-center" >Welcome</h1>
-      <h2 className="font-mono text-xl flex items-center justify-center">Hello! Explore my work and ongoing projects to learn more about my background, interests, and what I’m currently focused on.</h2>
+      <h2 className="welcomeText">Zachary Mooney | Software Developer | Web Programmer</h2>
+      {/* <h2 className="font-mono text-xl flex mt-5 items-center justify-center">Zachary Mooney | Software Developer/Web Programmer</h2> */}
+      {/* <h1 className="font-mono text-9xl flex items-center justify-center" >Welcome</h1> */}
+      <h1>Welcome</h1>
+      {/* <h2 className="font-mono text-xl flex items-center justify-center">Hello! Explore my work and ongoing projects to learn more about my background, interests, and what I’m currently focused on.</h2> */}
+      <h2 className="welcomeText">Hello! Explore my work and ongoing projects to learn more about my background, interests, and what I’m currently focused on.</h2>
     </div>
   );
 }
